75f43d02cd6861a3d600bbe5279257cbae3bca1b
arbocz marci <aaarby@gmail.com>
2017-12-23 18:01:29 +0100

    FeatureMap introduced.

---
+++ FeatureMap.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "FeatureMap.h"
+
+
+FeatureMap::FeatureMap()
+{
+}
+
+
+FeatureMap::~FeatureMap()
+{
+}
---
+++ FeatureMap.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class FeatureMap
+{
+public:
+	FeatureMap();
+	~FeatureMap();
+};
+
