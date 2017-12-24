6f6549d80282f11a3ae8b655071f0f18a2fb2a06
arbocz marci <aaarby@gmail.com>
2017-12-24 21:46:41 +0100

    new test code

--- neural network.cpp
+++ neural network.cpp
@@ -15,11 +15,6 @@
 #include "json.hpp"
 
 #include <iostream>
-#include <fstream>
-#include <thread>
-#include <chrono>
-#include <vector>
-#include <map>
 
 //using json = nlohmann::json;
 
@@ -28,33 +23,50 @@
 
 int main()
 {
-	MNISTInputClass input(60000);
+	MNISTInputClass input(10000);
 	BitXORInputClass bit(1000);
 
 	Timer timer;
 
 	try {
+		/*int i = 25;
+		int k = 5;
 
-		/*Matrix<float> mat(100, 1);
-		mat.fillValue(1);
-		Matrix<float> mat2(100, 100);
-		mat2.fillValue(1);
+		int modifier = 0;
+		if (k % 2 == 0) modifier = 1;
 
-		for (int i = 0; i < 10000; ++i) {
-			Matrix<float> res = mat2*mat;
-		}*/
+		Matrix<float> input(i, i);
+		input.fillValue(1);
+
+		Matrix<float> kernel(k, k);
+		kernel.fillValue(1);
+
+		Matrix<float> result(input.getCols() - 2 * floor(kernel.getCols() / 2) + modifier, input.getCols() - 2 * floor(kernel.getCols() / 2) + modifier);
+		result.fillValue(0);
+
+		convolve(
+			input.getData(),  input.getRows(),  input.getCols(),
+			kernel.getData(), kernel.getRows(), kernel.getCols(),
+			result.getData(), result.getRows(), result.getCols());
+
+		result.print();*/
+
+		Net conv(&input, CostFunction::CrossEntropy, Regularization::L2);
+		conv.createNewLayer(784, NeuronType::Input, LayerType::Input);
+		conv.createNewLayer(50,   LayerType::Convolutional, 5, 5);
+		conv.createNewLayer(10,  NeuronType::Sigmoid, LayerType::Output);
 		
 
-		Net network(&input, CostFunction::CrossEntropy);
+		/*Net network(&input, CostFunction::CrossEntropy, Regularization::L2);
 		network.createNewLayer(784,  NeuronType::Input,   LayerType::Input );
 		network.createNewLayer(100,  NeuronType::Sigmoid, LayerType::General);
 		network.createNewLayer(10,   NeuronType::Sigmoid, LayerType::Output);
 
 		timer.createTimePoint("before");
-		network.train(10, 10, 5.0f, 0.01f);
+		network.train(10, 10, 5.0f, 0.001f);
 		timer.createTimePoint("after");
 
-		timer.printTimeDifferenceSec("before", "after");
+		timer.printTimeDifferenceSec("before", "after");*/
 		//network.saveAs("mnist.json");
 
 
@@ -64,14 +76,14 @@ int main()
 		//cpy.train(10, 10, 1.0f);
 		
 
-		/*Net bitnet(&bit);
+		/*Net bitnet(&bit, CostFunction::CrossEntropy, Regularization::L2);
 		bitnet.createNewLayer(2,  NeuronType::Input,   LayerType::Input);
-		bitnet.createNewLayer(10, NeuronType::Sigmoid, LayerType::General);
+		bitnet.createNewLayer(4, NeuronType::Sigmoid, LayerType::General);
 		bitnet.createNewLayer(2,  NeuronType::Sigmoid, LayerType::Output);
 
-		bitnet.train(1000, 10, 0.1f);*/
+		bitnet.train(1000, 10, 0.1f, 0.01f);
 
-		//bitnet.saveAs("XOR.json");
+		bitnet.saveAs("XOR.json");*/
 	}
 	catch (const NeuralException& exc) {
 		std::cout << exc.getErrorMessage() << '\n';
@@ -87,3 +99,32 @@ int main()
     return 0;
 }
 
+
+//int* arr = new int[25 * 25];
+//for (int i = 0; i < 25 * 25; ++i) {
+//	arr[i] = 2;
+//}
+//int* kernel = new int[5 * 5];
+//for (int i = 0; i < 5 * 5; ++i) {
+//	kernel[i] = 2;
+//}
+//int* result = new int[21 * 21];
+//
+//convolve(arr, 25, 25, kernel, 5, 5, result);
+//
+//for (int i = 0; i < 21; ++i) {
+//	for (int j = 0; j < 21; ++j) {
+//		std::cout << result[j + i * 21] << ' ';
+//	}
+//	std::cout << '\n';
+//}
+//
+//delete[] arr;
+//delete[] kernel;
+//delete[] result;
+//
+//std::cout << "\n\nCALCULATIONS FINISHED...\n";
+//char ccg;
+//std::cin >> ccg;
+//
+//return 0;
