197a5ace8d2c6778642d0667576cabd63bf1a3cd
arbocz marci <aaarby@gmail.com>
2017-12-24 21:44:08 +0100

    FeatureMap assessed with basic functionality.

--- FeatureMap.cpp
+++ FeatureMap.cpp
@@ -2,10 +2,39 @@
 #include "FeatureMap.h"
 
 
-FeatureMap::FeatureMap()
+FeatureMap::FeatureMap(const int& kernelWidth, const int& kernelHeight, ConvolutionLayer* _parent)
+	: kernel(Matrix<float>(kernelHeight, kernelWidth))
+	, parent(_parent)
 {
+	const int prevActivations = _parent->getPreviousLayer()->getActivations().getRows();
+	const int inputSide = sqrt(prevActivations);
+	int resultSide = inputSide - 2 * floor(kernelWidth / 2);
+	
+	if (kernelWidth % 2 == 0) {
+		resultSide += 1;
+	}
+
+	result = Matrix<float>(resultSide, resultSide);
 }
 
+void FeatureMap::init()
+{
+	std::uniform_real_distribution<float> uni(-1, 1);
+
+	bias = uni(Random::getMT());
+
+	kernel.fillGauss(0, 1);
+}
+
+Matrix<float> FeatureMap::getKernel() const
+{
+	return kernel;
+}
+
+Matrix<float> FeatureMap::getResult() const
+{
+	return result;
+}
 
 FeatureMap::~FeatureMap()
 {
--- FeatureMap.h
+++ FeatureMap.h
@@ -1,9 +1,28 @@
 #pragma once
 
+#include "NeuralMath.h"
+#include "Layer.h"
+
+#include <random>
+
+class ConvolutionLayer;
+
 class FeatureMap
 {
 public:
-	FeatureMap();
+	FeatureMap(const int& kernelWidth, const int& kernelHeight, ConvolutionLayer* _parent);
+
+	void	init();
+
+	Matrix<float>  getKernel() const;
+	Matrix<float>  getResult() const;
+
 	~FeatureMap();
+private:
+	float			bias;
+	Matrix<float>	kernel;
+	Matrix<float>	result;
+
+	ConvolutionLayer* parent;
 };
 
